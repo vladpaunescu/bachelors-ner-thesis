b525355080bfffc63962671cced3c4a41720b1b1
Vlad Paunescu <vlad@ubuntu.(none)>
2013-04-10 22:48:29 +0300

    Created url

--- google_scholar/scrapers/social_sciences_top.py
+++ google_scholar/scrapers/social_sciences_top.py
@@ -8,9 +8,9 @@ class SocialSciencesTopPublications:
 
     def __init__(self):
         self.urlRoot = "http://scholar.google.com/citations"
-        self.query = {"language": {"hl" : "en"},
-                      "order" :  {"view_op" : "top_venues"},
-                      "category" : {"vq" : "soc"}
+        self.query = {"language":  {"key" : "hl" , "value" : "en"},
+                      "order" :    {"key" : "view_op" , "value" :"top_venues"},
+                      "category" : {"key" : "vq" , "value" : "soc"}
                      }
 
     def getTopPublications(self):
@@ -18,5 +18,21 @@ class SocialSciencesTopPublications:
 
 
     def buildUrl(self):
-        language
+        language = self.query["language"]
+        order = self.query["order"]
+        category = self.query["category"]
+        queries = [language, order, category]
+
+        queryStringList = ["{0}={1}".format(el["key"], el["value"]) for el in queries]
+        queryString = "&".join(queryStringList)
+
+        url = "".join([self.urlRoot, "?", queryString])
+
+        print url
+
+
+if __name__ == "__main__":
+    socialSciences = SocialSciencesTopPublications()
+    socialSciences.buildUrl()
+
 
