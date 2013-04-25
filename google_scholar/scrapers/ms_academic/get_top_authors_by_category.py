bbc94eee1b3f7fb895e0200dfb0f54d229c7cdf0
Vlad Paunescu <vlad.paunescu@gmail.com>
2013-04-25 19:13:12 +0300

    Some changes to shit

---
+++ google_scholar/scrapers/ms_academic/get_top_authors_by_category.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+"""
+    This class is used for retrieving top authors by category.
+    URL format is
+    http://academic.research.microsoft.com/json.svc/search?AppId=c4442c4f-22a6-461d-bca9-f199020a24ce&DomainID=22&OrderBy=Rank&ResultObjects=Author&StartIdx=0&EndIdx=99
+"""
